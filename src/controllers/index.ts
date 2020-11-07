64ca7ae9084c49dccf9e6e3710fecbaf92c058d8
Francois AUBEUT <contact@mexar.fr>
2020-11-07 13:52:52 +0100

    remove ping

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,5 @@
-export * from './ping.controller';
 export * from './articles.controller';
 export * from './users.controller';
 export * from './legal-texts.controller';
-export * from './tags-controller.controller';
+export * from './tags.controller';
 export * from './users-article.controller';
