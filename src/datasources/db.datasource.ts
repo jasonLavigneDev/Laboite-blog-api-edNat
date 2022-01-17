e4d6757db32977c5abe48aa30a6e648cb5e92d24
Bruno Boiget <bruno.boiget@ac-dijon.fr>
2022-01-17 16:54:13 +0100

    fix(config): add checks and logs for mongo configuration

--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -1,17 +1,30 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
-const config = {
-  url:`mongodb://${encodeURIComponent(process.env.MONGO_USER || "")}:${encodeURIComponent(process.env.MONGO_PASSWORD || "")}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`,
-  name: 'db',
-  connector: 'mongodb',
-  host: '',
-  port: 0,
-  user: '',
-  password: '',
-  database: '',
-  useNewUrlParser: true
-};
+let config = {}
+
+try {
+  config = {
+    url: process.env.MONGO_USER ?
+    `mongodb://${encodeURIComponent(process.env.MONGO_USER || "")}:${encodeURIComponent(process.env.MONGO_PASSWORD || "")}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT || 27017}/${process.env.MONGO_DATABASE}` :
+    `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT || 27017}/${process.env.MONGO_DATABASE}`,
+    name: 'db',
+    connector: 'mongodb',
+    host: '',
+    port: 0,
+    user: '',
+    password: '',
+    database: '',
+    useNewUrlParser: true
+  };
+} catch (err) {
+    console.log('! Error while parsing MongoDB configuration !')
+    console.log(err);
+    process.exit(1);
+}
+
+console.log(` - Connecting to MongoDB at ${process.env.MONGO_HOST}:${process.env.MONGO_PORT || 27017}, database: ${process.env.MONGO_DATABASE}`)
+if (process.env.MONGO_USER) console.log(`   Database user is ${process.env.MONGO_USER}`)
 
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
