3d9e38ff51fa94a397d2364ea119f8cb4688b03d
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2023-11-29 15:21:53 +0100

    WIP: en fait ca doit etre comme ca le fichier eslint

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: '@loopback/eslint-config',
+  'no-unused-vars': 'off',
+  rules: {
+    // Note: you must disable the base rule as it can report incorrect errors
+  },
+};
